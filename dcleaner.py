d83637792e41eafac3ac55860a38bcbf39adb480
P1N60 <victor.guldregn@gmail.com>
2025-05-22 17:35:09 +0200

    data cleaner update

--- dcleaner.py
+++ dcleaner.py
@@ -5,7 +5,20 @@ sorted_data = raw_data.sort_values('title')
 
 sorted_data.to_csv('data_sorted.csv', index=False)
 
-print(sorted_data)
+
+last_title = None
+cleaned_df = pd.DataFrame()
+for row in range(len(sorted_data)):
+    title_name = sorted_data.loc[row, 'title']
+    sales = sorted_data.loc[row, 'total_sales']
+    if title_name == last_title:
+        
+
+    
+    if sales:
+        
+    last_title = title_name
+
 
 
 
