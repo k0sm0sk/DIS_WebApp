f40160c18e7855378178372eec904e6da9f87ca4
P1N60 <victor.guldregn@gmail.com>
2025-05-22 15:54:15 +0200

    sorted data by game

--- dcleaner.py
+++ dcleaner.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
 raw_data = pd.read_csv('data_raw.csv')
-raw_data.groupby('title')
+sorted_data = raw_data.sort_values('title')
+
+sorted_data.to_csv('data_sorted.csv', index=False)
+
+print(sorted_data)
+
+
+
+
+
 
