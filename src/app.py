27d932c6ba3c99e6cf99fe99ef36e5e9b909f097
Vitus Nyvang Legarth <110633417+Vinyle02@users.noreply.github.com>
2025-06-04 14:59:32 +0200

    sqlite version

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 # We import render_template so we can render Jinja2 code, and request so we can handle POSTs
 # We import sqlite, likely we don't need to install any new library because this is a default Python library
 import psycopg2
+import sqlite3
 
 # ? "\psql -h localhost -U postgres -d dis_projekt"
 
@@ -40,6 +41,7 @@ def increment():
         
         
     if float(session['left_sales']) > float(session['right_sales']) and button=='right':
+        app.logger.info(f"died")
         return new_game()
         
     if float(session['left_sales']) <= float(session['right_sales']) and button=='right':
@@ -47,32 +49,34 @@ def increment():
         session['left_img'], session['left_name'],session['left_sales'] = random_game()
     
     if float(session['left_sales']) < float(session['right_sales']) and button=='left':
+        app.logger.info(f"died")
         return new_game()
     
     return redirect(url_for('game_counter'))
 
 @app.route('/random_game')
-def random_game(cur_sales = 0.5):
+def random_game(cur_sales = 0.9):
+
     app.logger.info(f"cur_sales = {cur_sales}, type = {type(cur_sales)}")
-    conn = psycopg2.connect(
-        dbname="dis_projekt",
-        user="postgres",
-        password="",
-        host="localhost",
-        port="5432"
-    )
+
+    conn = sqlite3.connect('../data/game_data.db')
+
     cur = conn.cursor()
     
+    cursor = conn.cursor()
     cur.execute(f"""
                 SELECT img,title,total_sales FROM game_data 
-                WHERE total_sales::FLOAT > {cur_sales} 
+                WHERE total_sales > {cur_sales} 
                 ORDER BY RANDOM() 
                 LIMIT 1;
                 """)
+    results = cursor.fetchall()
+
     rows = cur.fetchall()
     img = rows[0][0]
     name = rows[0][1]
     sales = rows[0][2]
+    app.logger.info(f"name = {name}, sales = {sales}")
     return (f"https://www.vgchartz.com{img}", f"{name}", f"{sales}")
 
 @app.route('/new_game', methods=['POST'])
